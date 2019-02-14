4a8ae7d3edcffa6217dde9469e17f5633b83fa72
A.D.Faris <adfaris@gmail.com>
2019-02-14 17:57:32 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 // import logo from './logo.svg';
-import './App.css'
+import './App.css';
 
 class App extends Component {
   render () {
     return (
       <header className='header'>
-          <div className='text-box'> 
-          <h1 className='heading-main-text'>
-          A.D.Faris 
-          </h1>
-          </div>
+        <div className='text-box'>
+          <h1 className='heading-main-text'>A.D.Faris</h1>
+        </div>
       </header>
     )
   }
