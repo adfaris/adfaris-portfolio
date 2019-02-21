94e581eee6206aea5f7ef5f691d44bb59a50f441
A.D.Faris <adfaris@gmail.com>
2019-02-20 23:05:57 -0500

    about

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ class App extends Component {
             </div>
 
             <div className='row'>
-              <div className='col-1-of-2'>Text content</div>
+              <div className='col-1-of-2'><h3 className='heading-tertiary'> You are going to love this page.</h3>
+                              <p className='paragraph'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              </div>
               <div className='col-1-of-2'>Image goes here</div>
             </div>
           </section>
